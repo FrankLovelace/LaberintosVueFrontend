943ffec6dc5393b9ee08fb7c0e306da088ccbd37
FrankLovelace <alexandrogallegos12@gmail.com>
2025-09-22 18:46:09 -0600

    proyectos

---
+++ src/views/ProyectosView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue'
+</script>
+
+<template>
+  <div>
+    <Navbar />
+    <main class="text-center py-20">
+      <h1 class="text-4xl font-bold text-gray-800">Nuestros Proyectos</h1>
+      <p class="mt-4 text-lg text-gray-600">Próximamente...</p>
+    </main>
+  </div>
+</template>
