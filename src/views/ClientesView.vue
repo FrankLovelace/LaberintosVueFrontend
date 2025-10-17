505d1fef4217e5f53364b437c9c4703f0315de21
FrankLovelace <alexandrogallegos12@gmail.com>
2025-10-17 11:23:33 -0600

    menos es mas

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -5,12 +5,5 @@ import Navbar from '@/components/Navbar.vue'
 
 <template>
   <Navbar />
-  <main class="pt-20 pb-10">
-    <section class="bg-white py-20">
-      <div class="container mx-auto px-6 text-center">
-        <h1 class="text-4xl font-bold text-gray-800">Clientes</h1>
-      </div>
-    </section>
-  </main>
   <LogoGallery />
 </template>
