e1e816d86bcbcd013ff9b8bd1ef3e2ca219969a0
FrankLovelace <alexandrogallegos12@gmail.com>
2025-10-17 11:17:57 -0600

    se quita repeticion de introduccion de pagina

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -9,10 +9,6 @@ import Navbar from '@/components/Navbar.vue'
     <section class="bg-white py-20">
       <div class="container mx-auto px-6 text-center">
         <h1 class="text-4xl font-bold text-gray-800">Clientes</h1>
-        <p class="mt-4 text-gray-600 max-w-2xl mx-auto">
-          Descubre las empresas y organizaciones que han confiado en Laberintos Praderas para
-          transformar sus espacios de juego.
-        </p>
       </div>
     </section>
   </main>
