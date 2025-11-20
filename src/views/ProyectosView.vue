8ecb8134b5b2bab177bf7891cd8676431ac3a909
FrankLovelace <alexandrogallegos12@gmail.com>
2025-11-20 11:23:36 -0600

    se simplifica la sección de proyectos

--- src/views/ProyectosView.vue
+++ src/views/ProyectosView.vue
@@ -6,9 +6,6 @@ import PortfolioView from '@/components/PortfolioView.vue'
 <template>
   <div>
     <Navbar />
-    <main class="text-center py-20">
-      <h1 class="text-4xl font-bold text-gray-800">Nuestros Proyectos</h1>
-      <PortfolioView />
-    </main>
+    <PortfolioView />
   </div>
 </template>
